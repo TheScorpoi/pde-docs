e728916bc7d8647d8584dae9a36574ac1b8000bf
TheScorpoi <sobral@ua.pt>
2023-10-13 18:28:09 +0100

    try deploy docusauros

--- dissertation-docs/docusaurus.config.js
+++ dissertation-docs/docusaurus.config.js
@@ -16,15 +16,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://TheScorpoi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pde-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TheScorpoi', // Usually your GitHub org/user name.
-  projectName: 'master-thesis-docusaurus', // Usually your repo name.
+  projectName: 'pde-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
