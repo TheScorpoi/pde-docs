68f1ee005893b310794846daae75debe01419842
TheScorpoi <sobral@ua.pt>
2023-10-13 18:36:41 +0100

    change base url

--- dissertation-docs/docusaurus.config.js
+++ dissertation-docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   url: 'https://TheScorpoi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pde-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
